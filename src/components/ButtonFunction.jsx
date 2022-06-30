a144c2e109660426f2c74395d96c22c3a6a04eec
Afrizal <afrizalbagas.03@gmail.com>
2022-06-30 12:42:02 +0700

    change material ui button

--- src/components/ButtonFunction.jsx
+++ src/components/ButtonFunction.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Button from "@mui/material/Button";
 
 const ButtonFunction = ({ onClick, text }) => {
-  return <button onClick={onClick}>{text}</button>;
+  return (
+    <Button
+      onClick={onClick}
+      variant="contained"
+      size="small"
+      style={{ "margin-left": 10 }}
+    >
+      {text}
+    </Button>
+  );
 };
 
 export default ButtonFunction;
